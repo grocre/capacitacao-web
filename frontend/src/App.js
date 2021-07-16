7dafaad23e1d1f777bc294ee07a4cac6902c8b3b
Vinícius Leitão <vinileitao2000@gmail.com>
2021-07-16 14:49:34 -0300

    Added Nav and Content components

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import Nav from "./Nav";
+import Content from "./Content";
+
+import "./styles/global.scss";
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Nav/>
+      <Content/>
+    </div>
+  );
+}
+
+export default App;
