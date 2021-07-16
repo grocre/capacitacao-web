a12363dced762baf5722303bb3a07ba129022795
Vinícius Leitão <vinileitao2000@gmail.com>
2021-07-16 14:48:39 -0300

    Created a Title component and imported Title.scss

---
+++ frontend/src/Title.js
@@ -0,0 +1,11 @@
+import "./styles/Title.scss"
+
+function Title(){
+    return (
+        <div id="title">
+            <h1>Controle de notícias</h1>
+        </div>
+    );
+}
+
+export default Title;
